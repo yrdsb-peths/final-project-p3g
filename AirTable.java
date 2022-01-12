98c244ccb03f744870f41adbd66ea6db9e5ded19
Shawn348973371 <348973371@gapps.yrdsb.ca>
2022-01-12 11:01:22 -0500

    Recursive drawing for the "background"
    I drew circles on the table

--- AirTable.java
+++ AirTable.java
@@ -13,6 +13,11 @@ public class AirTable extends Actor
     private int width = 800;
     private int height = 600;
     
+    private GreenfootImage table = new GreenfootImage(width,height);
+        
+    private Color rink = Color.ORANGE;
+    private Color border = Color.BLACK;
+    private Color lines = Color.GRAY;
     public AirTable()
     {
         image = drawLine();
@@ -21,23 +26,35 @@ public class AirTable extends Actor
     
     private GreenfootImage drawLine()
     {
-        GreenfootImage table = new GreenfootImage(width,height);
-        
-        table.setColor(Color.ORANGE);
+        table.setColor(rink);
         table.fillRect(0,0,width,height);
         
-        table.setColor(Color.BLACK);
+        
+        table.setColor(lines);
+        
+        //lines
+        table.fillRect((width-25)/2,0,25,height);
+        
+        //circles
+        drawCircles(2,width/2,height/2,200);
+        
+        //border
+        table.setColor(border);
         table.fillRect(0,0,10,height); //Left line
         table.fillRect(10,0,width-20,10); //Top line
         table.fillRect(10,height-10,width-20,height); //Bottom Line
         table.fillRect(width-10,0,width,height); //Right line
-        table.setColor(Color.GRAY);
         
-        //Left-side of table
-        table.fillOval(-60, height/2 - 90, 140,180);
+        //goal areas
+        table.setColor(lines);
+         
+        table.fillOval(-70, height/2 - 90, 140,180);
+        table.fillOval(730, height/2 - 90, 140,180);
         
-        //Right-side of table
-        table.fillOval(720, height/2 - 90, 140,180);
+        table.setColor(rink);
+        
+        table.fillOval(-63, height/2 - 81, 126,162);
+        table.fillOval(737, height/2 - 81, 126,162);
         
         /**
         //Left-side of table
@@ -51,5 +68,20 @@ public class AirTable extends Actor
         **/
         
         return table;
+    }
+    
+    private void drawCircles(int N, int x, int y, int radius){
+        if(N==0) return;
+        table.setColor(lines);
+        table.fillOval(x-radius/2,y-radius/2,radius,radius);
+        table.setColor(rink);
+        table.fillOval(x-radius*3/7,y-radius*3/7,radius*6/7,radius*6/7);
+        table.setColor(lines);
+        table.fillOval(x-radius/8,y-radius/8,radius/4,radius/4);
+        
+        drawCircles(N-1,x-radius,y-radius*3/4,radius/2);
+        drawCircles(N-1,x-radius,y+radius*3/4,radius/2);
+        drawCircles(N-1,x+radius,y-radius*3/4,radius/2);
+        drawCircles(N-1,x+radius,y+radius*3/4,radius/2);
     }
 }
