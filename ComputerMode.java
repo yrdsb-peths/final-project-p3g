82075198c5cec9c2ed573a9899b2497f4699a2f2
Nelson-Wong-335686747 <335686747@gapps.yrdsb.ca>
2022-01-16 22:56:49 -0500

    Background Music for ComputerMode

--- ComputerMode.java
+++ ComputerMode.java
@@ -2,11 +2,26 @@ import greenfoot.*;
 
 public class ComputerMode extends World  
 {
+    private GreenfootSound ComputerModeMusic = new GreenfootSound("Super Smash Bros Remix.mp3");
     
     public ComputerMode()
     {
         //Size of the world
         super(1200, 800, 1); 
-        
+    }
+    
+    public void act()
+    {
+        started();
+    }
+    
+    public void started()
+    {
+        ComputerModeMusic.playLoop();
+    }
+    
+    public void stopped()
+    {
+        ComputerModeMusic.stop();
     }
 }
