dce192e02025a87881c83905598762181c9497d1
Nelson-Wong-335686747 <335686747@gapps.yrdsb.ca>
2022-01-16 22:35:50 -0500

    Manually adding back the contents of a previous commit "goBack Button mouseclick option added"

---
+++ ComputerMode.java
@@ -0,0 +1,13 @@
+import greenfoot.*;
+
+public class ComputerMode extends World  
+{
+    Puck puck = new Puck();
+    
+    public ComputerMode()
+    {
+        //Size of the world
+        super(1200, 800, 1); 
+        
+    }
+}
